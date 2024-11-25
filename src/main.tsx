f4c6a34d57b29243a0a4676e282ce48966ec3c46
JayJay1024 <lygm1062@gmail.com>
2024-11-25 17:55:07 +0800

    add privy provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { PrivyProvider } from '@privy-io/react-auth'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PrivyProvider
+      appId='cm3sf7rpj003b12btluj2f1kx'
+      config={{
+        appearance: {
+          loginMessage: 'Please login to continue',
+        }
+      }}
+    >
+      <App />
+    </PrivyProvider>
   </StrictMode>,
 )
