d8f18ae2202252c079360b1b6cecbc453f052d88
JayJay1024 <lygm1062@gmail.com>
2024-11-26 15:35:45 +0800

    setup tailwind css

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ createRoot(document.getElementById('root')!).render(
       config={{
         appearance: {
           loginMessage: 'Please login to continue',
+          theme: 'dark',
         }
       }}
     >
